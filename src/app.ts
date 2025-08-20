5696ae728377144fa4cf794e4bb632e2ebe660f1
Ulises Rodriguez <hey@ulises.cc>
2025-08-20 03:42:30 -0700

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import transactionsRoutes from "./routes/transactionsRoutes.js";
 import kpiRoutes from "./routes/kpiRoutes.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/categories", categoryRoutes);
